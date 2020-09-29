90c415b328105e86d519270d36312df137a070e7
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-09-29 13:58:32 +0200

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -8,9 +8,11 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
 sudo apt-get --assume-yes install curl git zsh gnome-tweaks github-desktop yarn micro
+sudo apt-get install fonts-powerline
 sudo snap install code --classic
 sudo snap install vlc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 curl -o ~/.zshrc https://raw.githubusercontent.com/Mozart409/dotfiles/master/.zshrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
 nvm install 12

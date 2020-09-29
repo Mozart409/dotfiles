68fbae4cb58d0d45a96e15e9d7dc9ccc25bf05c4
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-09-29 13:29:23 +0200

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-echo "Hello, install packages now!"
+echo "Hello, installing packages now!"
 echo " "
 sudo apt-get update
 wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
-sudo apt-get --assume-yes install curl git zsh gnome-tweaks github-desktop
+sudo apt-get --assume-yes install curl git zsh gnome-tweaks github-desktop yarn
 sudo snap install code --classic
 sudo snap install vlc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -o ~/.zshrc https://github.com/Mozart409/dotfiles/blob/master/.zshrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+nvm install 12
+nvm use 12
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 cargo install alacritty
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -o ~/.zshrc https://github.com/Mozart409/dotfiles/blob/master/.zshrc

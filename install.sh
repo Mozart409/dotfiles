8ca15409dd6dc180a924ca4fd11bef5a51cbab2f
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-09-29 13:15:41 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello, install packages now!"
+echo " "
+sudo apt-get update
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
+sudo apt-get update
+sudo apt-get --assume-yes install curl git zsh gnome-tweaks github-desktop
+sudo snap install code vlc
+curl https://sh.rustup.rs -sSf | sh
+cargo install alacritty
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -o ~/.zshrc https://github.com/Mozart409/dotfiles/blob/master/.zshrc

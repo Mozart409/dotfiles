b885618b852a153748a506f60ffc2e595d703bcf
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-11-22 15:48:36 +0100

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 echo "Hello, installing packages now!"
 echo " "
-sudo apt-get update
-wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
+sudo apt-get update -y
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
-sudo apt-get --assume-yes install curl git zsh gnome-tweaks github-desktop yarn micro
+sudo apt install github-desktop -y
+sudo apt-get --assume-yes install curl git zsh gnome-tweaks
 sudo apt-get install fonts-powerline
 sudo snap install code --classic
 sudo snap install vlc
@@ -15,9 +14,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/buonomo/yarn-completion ~/.oh-my-zsh/custom/plugins/yarn-completion
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 curl -o ~/.zshrc https://raw.githubusercontent.com/Mozart409/dotfiles/master/.zshrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
-nvm install 12
-nvm use 12
-curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
-cargo install alacritty
+curl -fsSL https://fnm.vercel.app/install | bash
+curl https://getmic.ro | bash
